f947b1a9ce9fa5042c59ec8d6e0b1603c5f384ed
wuyichen24 <wuyichen24@gmail.com>
2018-11-06 10:43:26 -0800

    Minor change for config server

--- server/config-server/src/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
+++ server/config-server/src/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -9,9 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
-
 }
