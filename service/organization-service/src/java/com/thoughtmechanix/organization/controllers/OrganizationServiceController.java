3aa61200357aadcae0ced2aa28df342200a066a4
wuyichen24 <wuyichen24@gmail.com>
2018-11-12 16:54:36 -0800

    Minor code change

--- service/organization-service/src/java/com/thoughtmechanix/organization/controllers/OrganizationServiceController.java
+++ service/organization-service/src/java/com/thoughtmechanix/organization/controllers/OrganizationServiceController.java
@@ -21,7 +21,7 @@ public class OrganizationServiceController {
     private static final Logger logger = LoggerFactory.getLogger(OrganizationServiceController.class);
 
     @RequestMapping(value="/{organizationId}",method = RequestMethod.GET)
-    public Organization getOrganization( @PathVariable("organizationId") String organizationId) {
+    public Organization getOrganization(@PathVariable("organizationId") String organizationId) {
         logger.debug("Looking up data for org {}", organizationId);
 
         Organization org = orgService.getOrg(organizationId);
@@ -30,18 +30,18 @@ public class OrganizationServiceController {
     }
 
     @RequestMapping(value="/{organizationId}",method = RequestMethod.PUT)
-    public void updateOrganization( @PathVariable("organizationId") String orgId, @RequestBody Organization org) {
-        orgService.updateOrg( org );
+    public void updateOrganization(@PathVariable("organizationId") String orgId, @RequestBody Organization org) {
+        orgService.updateOrg(org);
     }
 
     @RequestMapping(value="/{organizationId}",method = RequestMethod.POST)
     public void saveOrganization(@RequestBody Organization org) {
-       orgService.saveOrg( org );
+       orgService.saveOrg(org);
     }
 
-    @RequestMapping(value="/{organizationId}",method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{organizationId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteOrganization( @PathVariable("orgId") String orgId,  @RequestBody Organization org) {
-        orgService.deleteOrg( org );
+    public void deleteOrganization(@PathVariable("orgId") String orgId,  @RequestBody Organization org) {
+        orgService.deleteOrg(org);
     }
 }
