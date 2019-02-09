d7cc90b9675158afd5a80f6f5d4e72797ca178cb
wuyichen24 <wuyichen24@gmail.com>
2019-02-09 14:27:34 -0800

    Refactoring config server

--- chapters/ch1/src/java/com/thoughtmechanix/simpleservice/Application.java
+++ chapters/ch1/src/java/com/thoughtmechanix/simpleservice/Application.java
@@ -7,18 +7,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@SpringBootApplication
-@RestController
-@RequestMapping(value="hello")
+@SpringBootApplication           // Tell the Spring Boot framework that this class is the entry point for the Spring Boot service.
+@RestController                  // Tell the Spring Boot you're going to expose the code in this class as a Spring RestController class.
+@RequestMapping(value="hello")   // All URLs exposed in this application will be prefaced with /hello prefix.
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @RequestMapping(value="/{firstName}/{lastName}",method = RequestMethod.GET)
-    public String hello( @PathVariable("firstName") String firstName,
-                         @PathVariable("lastName") String lastName) {
+    @RequestMapping(value="/{firstName}/{lastName}",method = RequestMethod.GET)                                    // Spring Boot will expose an endpoint as a GET-based REST endpoint that 
+                                                                                                                   // will take two parameters: firstName and lastName.
+    public String hello(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {  // Maps the firstName and lastName parameters passed in on the URL to 
+    	                                                                                                               // two variables passed into the hello function.     
 
-        return String.format("{\"message\":\"Hello %s %s\"}", firstName, lastName);
+        return String.format("{\"message\":\"Hello %s %s\"}", firstName, lastName);                                // Returns a simple JSON string that you manually build.
     }
 }
--- server/config-server/src/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
+++ server/config-server/src/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -3,10 +3,8 @@ package com.thoughtmechanix.confsvr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
 	public static void main(String[] args) {
