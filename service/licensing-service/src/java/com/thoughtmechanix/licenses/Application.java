a7e64038aa13d59e8ffd97fd632dcc313bdabc89
wuyichen24 <wuyichen24@gmail.com>
2019-02-11 00:26:44 -0800

    retire redis for licensing service

--- service/licensing-service/src/java/com/thoughtmechanix/licenses/Application.java
+++ service/licensing-service/src/java/com/thoughtmechanix/licenses/Application.java
@@ -13,8 +13,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
@@ -22,35 +22,35 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @SpringBootApplication
 @RefreshScope
-@EnableEurekaClient
-@EnableCircuitBreaker
-@EnableBinding(Sink.class)
-@EnableResourceServer
+//@EnableEurekaClient
+//@EnableCircuitBreaker
+//@EnableBinding(Sink.class)
+//@EnableResourceServer
 public class Application {
-    @Autowired
-    private ServiceConfig serviceConfig;
-
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
-
-    @Bean
-    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
-        return new OAuth2RestTemplate(details, oauth2ClientContext);
-    }
-
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConnFactory = new JedisConnectionFactory();
-        jedisConnFactory.setHostName(serviceConfig.getRedisServer());
-        jedisConnFactory.setPort(serviceConfig.getRedisPort() );
-        return jedisConnFactory;
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
-    }
+//    @Autowired
+//    private ServiceConfig serviceConfig;
+//
+//    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+//
+//    @Bean
+//    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+//        return new OAuth2RestTemplate(details, oauth2ClientContext);
+//    }
+//
+//    @Bean
+//    public JedisConnectionFactory jedisConnectionFactory() {
+//        JedisConnectionFactory jedisConnFactory = new JedisConnectionFactory();
+//        jedisConnFactory.setHostName(serviceConfig.getRedisServer());
+//        jedisConnFactory.setPort(serviceConfig.getRedisPort() );
+//        return jedisConnFactory;
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(jedisConnectionFactory());
+//        return template;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
