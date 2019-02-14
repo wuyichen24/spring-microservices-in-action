62d3821f74525a52e7b76b122be21cd59c1c7020
wuyichen24 <wuyichen24@gmail.com>
2019-02-14 01:16:47 -0800

    Recover some java classes in licensing service

---
+++ service/licensing-service/src/java/com/thoughtmechanix/licenses/events/CustomChannels.java
@@ -0,0 +1,9 @@
+package com.thoughtmechanix.licenses.events;
+
+//import org.springframework.cloud.stream.annotation.Input;
+//import org.springframework.messaging.SubscribableChannel;
+
+public interface CustomChannels {
+//    @Input("inboundOrgChanges")
+//    SubscribableChannel orgs();
+}
