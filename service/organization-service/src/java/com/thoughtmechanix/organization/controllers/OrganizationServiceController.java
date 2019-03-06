59c98cb2b1417f1908df276cfa3d1352a780c852
wuyichen24 <wuyichen24@gmail.com>
2019-03-06 12:53:43 -0800

    Fixed the method not found issue in organization service

--- service/organization-service/src/java/com/thoughtmechanix/organization/controllers/OrganizationServiceController.java
+++ service/organization-service/src/java/com/thoughtmechanix/organization/controllers/OrganizationServiceController.java
@@ -16,32 +16,32 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RestController
 @RequestMapping(value="v1/organizations")
 public class OrganizationServiceController {
-//    @Autowired
-//    private OrganizationService orgService;
-//    private static final Logger logger = LoggerFactory.getLogger(OrganizationServiceController.class);
-//
-//    @RequestMapping(value="/{organizationId}",method = RequestMethod.GET)
-//    public Organization getOrganization(@PathVariable("organizationId") String organizationId) {
-//        logger.debug("Looking up data for org {}", organizationId);
-//
-//        Organization org = orgService.getOrg(organizationId);
-//        org.setContactName(org.getContactName());
-//        return org;
-//    }
-//
-//    @RequestMapping(value="/{organizationId}",method = RequestMethod.PUT)
-//    public void updateOrganization(@PathVariable("organizationId") String orgId, @RequestBody Organization org) {
-//        orgService.updateOrg(org);
-//    }
-//
-//    @RequestMapping(value="/{organizationId}",method = RequestMethod.POST)
-//    public void saveOrganization(@RequestBody Organization org) {
-//       orgService.saveOrg(org);
-//    }
-//
-//    @RequestMapping(value = "/{organizationId}", method = RequestMethod.DELETE)
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public void deleteOrganization(@PathVariable("orgId") String orgId,  @RequestBody Organization org) {
-//        orgService.deleteOrg(org);
-//    }
+    @Autowired
+    private OrganizationService orgService;
+    private static final Logger logger = LoggerFactory.getLogger(OrganizationServiceController.class);
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.GET)
+    public Organization getOrganization(@PathVariable("organizationId") String organizationId) {
+        logger.debug("Looking up data for org {}", organizationId);
+
+        Organization org = orgService.getOrg(organizationId);
+        org.setContactName(org.getContactName());
+        return org;
+    }
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.PUT)
+    public void updateOrganization(@PathVariable("organizationId") String orgId, @RequestBody Organization org) {
+        orgService.updateOrg(org);
+    }
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.POST)
+    public void saveOrganization(@RequestBody Organization org) {
+       orgService.saveOrg(org);
+    }
+
+    @RequestMapping(value = "/{organizationId}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrganization(@PathVariable("orgId") String orgId,  @RequestBody Organization org) {
+        orgService.deleteOrg(org);
+    }
 }
