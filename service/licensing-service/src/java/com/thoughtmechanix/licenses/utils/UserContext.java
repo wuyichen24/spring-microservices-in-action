ceaed824dc90faa7a4e1514a9769d75cbfcd7278
wuyichen24 <wuyichen24@gmail.com>
2019-03-15 11:57:10 -0700

    Format UserContext

--- service/licensing-service/src/java/com/thoughtmechanix/licenses/utils/UserContext.java
+++ service/licensing-service/src/java/com/thoughtmechanix/licenses/utils/UserContext.java
@@ -2,6 +2,12 @@ package com.thoughtmechanix.licenses.utils;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * @author  Wuyi Chen
+ * @date    03/14/2019
+ * @version 1.0
+ * @since   1.0
+ */
 @Component
 public class UserContext {
     public static final String CORRELATION_ID = "tmx-correlation-id";
@@ -9,38 +15,17 @@ public class UserContext {
     public static final String USER_ID        = "tmx-user-id";
     public static final String ORG_ID         = "tmx-org-id";
 
-    private String correlationId= new String();
-    private String authToken= new String();
-    private String userId = new String();
-    private String orgId = new String();
+    private String correlationId = "";
+    private String authToken     = "";
+    private String userId        = "";
+    private String orgId         = "";
 
-    public String getCorrelationId() { return correlationId;}
-    public void setCorrelationId(String correlationId) {
-        this.correlationId = correlationId;
-    }
-
-    public String getAuthToken() {
-        return authToken;
-    }
-
-    public void setAuthToken(String authToken) {
-        this.authToken = authToken;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getOrgId() {
-        return orgId;
-    }
-
-    public void setOrgId(String orgId) {
-        this.orgId = orgId;
-    }
-
+    public String getCorrelationId()                     { return correlationId;               }
+    public void   setCorrelationId(String correlationId) { this.correlationId = correlationId; }
+    public String getAuthToken()                         { return authToken;                   }
+    public void   setAuthToken(String authToken)         { this.authToken = authToken;         }
+    public String getUserId()                            { return userId;                      }
+    public void   setUserId(String userId)               { this.userId = userId;               }
+    public String getOrgId()                             { return orgId;                       }
+    public void   setOrgId(String orgId)                 { this.orgId = orgId;                 }
 }
