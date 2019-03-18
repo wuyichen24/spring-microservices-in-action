db8f1a76bbcde8cfad79c67978cab0343e854031
wuyichen24 <wuyichen24@gmail.com>
2019-03-18 16:17:15 -0700

    Enable the basic zuul server

--- server/zuul-server/src/java/com/thoughtmechanix/zuulsvr/ZuulServerApplication.java
+++ server/zuul-server/src/java/com/thoughtmechanix/zuulsvr/ZuulServerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableZuulProxy
+@EnableZuulProxy                    // Enables the service to be a Zuul server
 public class ZuulServerApplication {
     @LoadBalanced
     @Bean
