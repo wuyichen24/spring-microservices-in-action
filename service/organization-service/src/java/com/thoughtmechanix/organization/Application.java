894ff32b92ba7e7702c2dc34ea1fdc746191cc8b
wuyichen24 <wuyichen24@gmail.com>
2019-04-06 15:47:05 -0700

    Let organization serivce be protected by OAuth2

--- service/organization-service/src/java/com/thoughtmechanix/organization/Application.java
+++ service/organization-service/src/java/com/thoughtmechanix/organization/Application.java
@@ -3,12 +3,12 @@ package com.thoughtmechanix.organization;
 import com.thoughtmechanix.organization.utils.UserContextFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.stream.annotation.EnableBinding;
 //import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 //import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import javax.servlet.Filter;
 
@@ -16,13 +16,13 @@ import javax.servlet.Filter;
 @EnableEurekaClient
 //@EnableCircuitBreaker
 //@EnableBinding(Source.class)
-//@EnableResourceServer
+@EnableResourceServer                           // This is the protected service by OAuth2
 public class Application {
-//    @Bean
-//    public Filter userContextFilter() {
-//        UserContextFilter userContextFilter = new UserContextFilter();
-//        return userContextFilter;
-//    }
+    @Bean
+    public Filter userContextFilter() {
+        UserContextFilter userContextFilter = new UserContextFilter();
+        return userContextFilter;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- service/organization-service/src/java/com/thoughtmechanix/organization/security/ResourceServerConfiguration.java
+++ service/organization-service/src/java/com/thoughtmechanix/organization/security/ResourceServerConfiguration.java
@@ -3,19 +3,27 @@ package com.thoughtmechanix.organization.security;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-//@Configuration
-//public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-//
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception{
-//        http
-//                .authorizeRequests()
+/**
+ * The class to define the access control rules.
+ * 
+ * @author  Wuyi Chen
+ * @date    04/06/2019
+ * @version 1.0
+ * @since   1.0
+ */
+@Configuration
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
 //                .antMatchers(HttpMethod.DELETE, "/v1/organizations/**")
 //                .hasRole("ADMIN")
-//                .anyRequest()
-//                .authenticated();
-//    }
-//}
+                .anyRequest()
+                .authenticated();
+    }
+}
