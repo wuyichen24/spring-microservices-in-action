62fdea3334cd11c4b799f485608e4a430ce25c06
wuyichen24 <wuyichen24@gmail.com>
2019-04-09 17:23:01 -0700

    Add some comments

--- server/zuul-server/src/java/com/thoughtmechanix/zuulsvr/config/ServiceConfig.java
+++ server/zuul-server/src/java/com/thoughtmechanix/zuulsvr/config/ServiceConfig.java
@@ -4,15 +4,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+/**
+ * The configuration class for Zuul server.
+ * 
+ * <p>The purpose of this class is mainly to load the JWT signing key from 
+ * the application configuration file.
+ * 
+ * @author  Wuyi Chen
+ * @date    04/10/2019
+ * @version 1.0
+ * @since   1.0
+ */
 @Component
 @Configuration
 public class ServiceConfig {
-  @Value("${signing.key}")
-  private String jwtSigningKey="";
+	@Value("${signing.key}")
+	private String jwtSigningKey = "";
 
-
-  public String getJwtSigningKey() {
-    return jwtSigningKey;
-  }
-
+	public String getJwtSigningKey() {
+		return jwtSigningKey;
+	}
 }
