1b3f5024f52a3496dbfaf32c529631280d5aa3b0
wuyichen24 <wuyichen24@gmail.com>
2019-05-10 14:36:19 -0700

    change the log level to debug for licensing service

--- server/zuul-server/src/java/com/thoughtmechanix/zuulsvr/ZuulServerApplication.java
+++ server/zuul-server/src/java/com/thoughtmechanix/zuulsvr/ZuulServerApplication.java
@@ -1,8 +1,5 @@
 package com.thoughtmechanix.zuulsvr;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -10,11 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.sleuth.Sampler;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
-
-import com.thoughtmechanix.zuulsvr.utils.UserContextInterceptor;
 
 /**
  * The bootstrap class for the zuul server.
