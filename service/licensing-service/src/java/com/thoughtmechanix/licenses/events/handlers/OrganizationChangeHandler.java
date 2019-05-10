6c66305c7cc53a4ce13823d984acadd2ef7a283d
wuyichen24 <wuyichen24@gmail.com>
2019-05-10 14:53:27 -0700

    Add the logic of deleting cached record from Redis

--- service/licensing-service/src/java/com/thoughtmechanix/licenses/events/handlers/OrganizationChangeHandler.java
+++ service/licensing-service/src/java/com/thoughtmechanix/licenses/events/handlers/OrganizationChangeHandler.java
@@ -14,6 +14,11 @@ import org.springframework.cloud.stream.messaging.Sink;
  * The message handler for receiving the message of the organization change 
  * from the message queue.
  * 
+ * <p>If the licensing service receives a message about a change on 
+ * a organization record. the licensing service will delete the cached 
+ * organization record in Redis (because the record cached before is 
+ * out-of-date) 
+ * 
  * @author  Wuyi Chen
  * @date    05/08/2019
  * @version 1.0
@@ -21,34 +26,34 @@ import org.springframework.cloud.stream.messaging.Sink;
  */
 @EnableBinding(Sink.class)
 public class OrganizationChangeHandler {
-//    @Autowired
-//    private OrganizationRedisRepository organizationRedisRepository;
+    @Autowired
+    private OrganizationRedisRepository organizationRedisRepository;
 
     private static final Logger logger = LoggerFactory.getLogger(OrganizationChangeHandler.class);
 
     @StreamListener(Sink.INPUT)
     public void loggerSink(OrganizationChangeModel orgChange) {
     	logger.info("Received an event for organization id {}", orgChange.getOrganizationId());
-//        
-//        switch(orgChange.getAction()){
-//            case "GET":
-//                logger.debug("Received a GET event from the organization service for organization id {}", orgChange.getOrganizationId());
-//                break;
-//            case "SAVE":
-//                logger.debug("Received a SAVE event from the organization service for organization id {}", orgChange.getOrganizationId());
-//                break;
-////            case "UPDATE":
-////                logger.debug("Received a UPDATE event from the organization service for organization id {}", orgChange.getOrganizationId());
-////                organizationRedisRepository.deleteOrganization(orgChange.getOrganizationId());
-////                break;
-////            case "DELETE":
-////                logger.debug("Received a DELETE event from the organization service for organization id {}", orgChange.getOrganizationId());
-////                organizationRedisRepository.deleteOrganization(orgChange.getOrganizationId());
-////                break;
-//            default:
-//                logger.error("Received an UNKNOWN event from the organization service of type {}", orgChange.getType());
-//                break;
-//
-//        }
+    	        
+        switch(orgChange.getAction()){
+            case "GET":
+                logger.debug("Received a GET event from the organization service for organization id {}", orgChange.getOrganizationId());
+                break;
+            case "SAVE":
+                logger.debug("Received a SAVE event from the organization service for organization id {}", orgChange.getOrganizationId());
+                break;
+            case "UPDATE":
+                logger.debug("Received a UPDATE event from the organization service for organization id {}", orgChange.getOrganizationId());
+                organizationRedisRepository.deleteOrganization(orgChange.getOrganizationId());
+                break;
+            case "DELETE":
+                logger.debug("Received a DELETE event from the organization service for organization id {}", orgChange.getOrganizationId());
+                organizationRedisRepository.deleteOrganization(orgChange.getOrganizationId());
+                break;
+            default:
+                logger.error("Received an UNKNOWN event from the organization service of type {}", orgChange.getType());
+                break;
+
+        }
     }
 }
