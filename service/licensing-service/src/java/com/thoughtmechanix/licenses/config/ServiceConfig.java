0054518e57c1835766f345a510f1175b3e3d7419
wuyichen24 <wuyichen24@gmail.com>
2019-05-09 17:50:04 -0700

    Remove unuse parameter in ServiceConfig of licensing service

--- service/licensing-service/src/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
+++ service/licensing-service/src/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
@@ -5,18 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceConfig{
-//	@Value("${example.property}")
-	private String exampleProperty="";
 
 //	@Value("${redis.server}")
 	private String redisServer="";
 
 //	@Value("${redis.port}")
 	private String redisPort="";
-
-	public String getExampleProperty(){
-		return exampleProperty;
-	}
 
 	public String getRedisServer(){
 		return redisServer;
