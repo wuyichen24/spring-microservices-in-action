c0948a885695300d66fee9c8721f90fa1ca614a3
wuyichen24 <wuyichen24@gmail.com>
2019-05-09 20:18:30 -0700

    Fix the compiling issue

--- service/licensing-service/src/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
+++ service/licensing-service/src/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
@@ -5,12 +5,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceConfig{
+//	@Value("${example.property}")
+	private String exampleProperty="";
 
 //	@Value("${redis.server}")
 	private String redisServer="";
 
 //	@Value("${redis.port}")
 	private String redisPort="";
+
+	public String getExampleProperty(){
+		return exampleProperty;
+	}
 
 	public String getRedisServer(){
 		return redisServer;
